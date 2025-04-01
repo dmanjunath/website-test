9046079a74611c3bbdc0a35f963196db3d1cb02a
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2025-04-01 16:27:33 -0400

    switch api to mocky

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ setTimeout(() => {
 }, 1000)
 
 setInterval(() => {
-  fetch('https://dashboard-server-sepolia.earthfast.com/health/server')
+  fetch('https://run.mocky.io/v3/134dcef7-83ba-4479-b405-c6de417fe4e9')
     .then(res => res.json())
     .then(data => {
       console.log('health', data);
