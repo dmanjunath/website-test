1aef25a75e99a6dbba448fe134495e99ab7d39f0
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2025-04-01 16:11:06 -0400

    more interaction

--- main.js
+++ main.js
@@ -8,5 +8,14 @@ setTimeout(() => {
   button.addEventListener('click', () => {
     count++;
     counter.innerText = count;
+    console.log('clicked', count);
   });
 }, 1000)
+
+setInterval(() => {
+  fetch('https://dashboard-server-sepolia.earthfast.com/health/server')
+    .then(res => res.json())
+    .then(data => {
+      console.log('health', data);
+    });
+}, 10_000);
